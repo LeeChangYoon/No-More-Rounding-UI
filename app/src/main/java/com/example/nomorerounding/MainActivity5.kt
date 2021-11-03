221beaba59347c702183c2a6e6bf589019ddfe08
TaegyuCho <wh2446@naver.com>
2021-11-03 23:10:39 +0900

    add pmc5(main page)

---
+++ app/src/main/java/com/example/nomorerounding/MainActivity5.kt
@@ -0,0 +1,11 @@
+package com.example.nomorerounding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pmc5)
+    }
+}
